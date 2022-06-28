ea8a63f62b57c566abf7a8a6875d149db357c9d4
Noire001 <noiredev@protonmail.com>
2022-06-29 01:41:10 +0300

    Add EF, Serilog

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,28 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Host.UseSerilog((context, services, configuration) => configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .Enrich.FromLogContext());
+
+Log.Information("Starting web host");
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    try
+    {
+        var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+        await context.Database.MigrateAsync();
+    }
+    catch (Exception e)
+    {
+        Log.Error(e, "Error while migrating database");
+    }
+}
 
 app.MapGet("/", () => "Hello World!");
 
