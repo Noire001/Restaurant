a2116dc5337fecaefa1f4771660a865ac617d174
Noire001 <noiredev@protonmail.com>
2022-06-29 01:41:10 +0300

    Add EF, Serilog

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using Serilog;
+using Serilog.Events;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Host.UseSerilog((context, services, configuration) => configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .Enrich.FromLogContext());
+
+Log.Information("Starting web host");
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    try
+    {
+        var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+        await context.Database.MigrateAsync();
+    }
+    catch (Exception e)
+    {
+        Log.Error(e, "Error while migrating database");
+    }
+}
 
 app.MapGet("/", () => "Hello World!");
 
---
+++ Domain/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain;
+
+public class Item
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = null!;
+    public float Price { get; set; }
+    public string Description { get; set; } = null!;
+}
